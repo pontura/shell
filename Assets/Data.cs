206ba7011a9a153f6af32e1f7cb1e8a3464da85f
Agustin Pontura <pontura@gmail.com>
2021-06-01 14:11:23 -0300

    empty

---
+++ Assets/Data.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Data : MonoBehaviour
+{
+    const string PREFAB_PATH = "Data";
+    static Data mInstance = null;
+    public bool DEBUG;
+    public string newScene;
+
+    public static Data Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = FindObjectOfType<Data>();
+
+                if (mInstance == null)
+                {
+                    GameObject go = Instantiate(Resources.Load<GameObject>(PREFAB_PATH)) as GameObject;
+                    mInstance = go.GetComponent<Data>();
+                }
+            }
+            return mInstance;
+        }
+    }
+    public void LoadLevel(string aLevelName)
+    {
+        this.newScene = aLevelName;
+         SceneManager.LoadScene(newScene);
+    }
+    void Awake()
+    {
+        if (!mInstance)
+            mInstance = this;
+
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/Events.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Events
+{
+    public static System.Action ResetApp = delegate { };
+    
+    public static System.Action<string> GotoTo = delegate { };
+    public static System.Action<string> GotoBackTo = delegate { };
+    public static System.Action Back = delegate { };
+
+}
+   
